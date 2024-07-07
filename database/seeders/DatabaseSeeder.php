1f8650171c7df4059a54ca2b67a161d21052fed3
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2024-07-07 12:49:35 +0700

    modified seeder field

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,9 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Operator',
+            'username' => 'operator',
+            'password' => 'operator',
         ]);
     }
 }
