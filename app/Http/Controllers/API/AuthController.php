2dd0dcfac81f2f4c1d8152dc2893e7ab4df64bf7
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2024-07-07 13:35:01 +0700

    create method for login and logout

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -2,10 +2,42 @@
 
 namespace App\Http\Controllers\API;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    //
+    public function login(Request $request)
+    {
+        $credentials = $request->only('username', 'password');
+
+        $user = Auth::attempt($credentials);
+        try {
+            if ($user) {
+                $token = $request->user()->createToken('authToken')->plainTextToken;
+                return response()->json([
+                    'token' => $token,
+                    'status' => 200,
+                    'message' => 'Login successful'
+                ], 200);
+            }
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => $e->getMessage(),
+                'status' => $e->getCode(),
+            ], 401);
+        }
+
+        return response()->json(['error' => 'Unauthorized'], 401);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+        return response()->json([
+            'message' => 'Logged out successfully',
+            'status' => 200,
+        ], 200);
+    }
 }
