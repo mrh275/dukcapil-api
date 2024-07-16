c690821ae1b1580a02a1dd164b35eae0a7bcbf6b
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2024-07-17 00:45:56 +0700

    modified seeder instances

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,22 +16,12 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Operator',
-            'username' => 'operator',
-            'password' => 'operator',
-        ]);
+        // User::factory()->create([
+        //     'name' => 'Operator',
+        //     'username' => 'operator',
+        //     'password' => 'operator',
+        // ]);
 
-        DataPenduduk::factory()->sequence(
-            ['tanggal_lahir' => fake()->randomElement([2008, 2009, 2010]) . '-' . fake()->month() . '-' . fake()->dayOfMonth(),],
-            ['pekerjaan' => fake()->randomElement(['Pelajar', 'Mahasiswa']),],
-            ['status_perkawinan' => 'Belum Kawin'],
-            ['status_dalam_keluarga' => 'Anak'],
-            ['kewarganegaraan' => 'WNI'],
-            ['no_paspor' => null],
-            ['no_kitap' => null],
-            ['nama_ayah' => null],
-            ['nama_ibu' => null],
-        )->count(50)->create();
+        DataPenduduk::factory()->count(100)->create();
     }
 }
