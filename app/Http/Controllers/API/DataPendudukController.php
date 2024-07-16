92f8490471cda675cf6df1f5b2379df9f7229516
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2024-07-17 00:51:13 +0700

    created method for handling get data penduduk endpoint

--- app/Http/Controllers/API/DataPendudukController.php
+++ app/Http/Controllers/API/DataPendudukController.php
@@ -2,10 +2,33 @@
 
 namespace App\Http\Controllers\API;
 
-use App\Http\Controllers\Controller;
+use App\Models\DataPenduduk;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class DataPendudukController extends Controller
 {
-    //
+    public function getPenduduk($nik)
+    {
+        try {
+            $penduduk = DataPenduduk::where('nik', $nik)->first();
+            if ($penduduk) {
+                return response()->json([
+                    'status' => 'success',
+                    'message' => 'Data penduduk ditemukan',
+                    'data' => $penduduk,
+                ]);
+            } else {
+                return response()->json([
+                    'status' => 'error',
+                    'message' => 'Data penduduk tidak ditemukan',
+                ]);
+            }
+        } catch (\Exception $e) {
+            return response()->json([
+                'status' => $e->getCode(),
+                'message' => $e->getMessage(),
+            ]);
+        }
+    }
 }
