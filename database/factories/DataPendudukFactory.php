394042645deee58831365fca6dcf5954635b1624
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2024-07-16 21:29:24 +0700

    create faker scenario for data penduduk

--- database/factories/DataPendudukFactory.php
+++ database/factories/DataPendudukFactory.php
@@ -17,7 +17,28 @@ class DataPendudukFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'nik' => $this->faker->unique()->numerify('321613##########'),
+            'nama' => $this->faker->name(),
+            'tempat_lahir' => $this->faker->randomElement(['Karawang', 'Bekasi', 'Subang', 'Purwakarta']),
+            'tanggal_lahir' => $this->faker->date(),
+            'jenis_kelamin' => $this->faker->randomElement(['L', 'P']),
+            'alamat' => $this->faker->address(),
+            'rt' => $this->faker->numerify('###'),
+            'rw' => $this->faker->numerify('###'),
+            'kelurahan' => $this->faker->randomElement(['Cikampek Utara', 'Cikampek Selatan', 'Dawuan Barat', 'Dawuan Tengah', 'Dawuan Timur']),
+            'kecamatan' => 'Cikampek',
+            'agama' => $this->faker->randomElement(['Islam', 'Kristen']),
+            'pendidikan' => $this->faker->randomElement(['Tidak Sekolah', 'SD', 'SMP', 'SMA', 'D3', 'S1', 'S2', 'S3']),
+            'pekerjaan' => $this->faker->jobTitle(),
+            'golongan_darah' => $this->faker->randomElement(['A', 'B', 'AB', 'O']),
+            'status_perkawinan' => $this->faker->randomElement(['Kawin', 'Belum Kawin', 'Cerai Hidup', 'Cerai Mati']),
+            'tanggal_perkawinan' => $this->faker->date(),
+            'status_dalam_keluarga' => $this->faker->randomElement(['Kepala Keluarga', 'Istri', 'Anak']),
+            'kewarganegaraan' => $this->faker->randomElement(['WNI', 'WNA']),
+            'no_paspor' => $this->faker->numerify('############'),
+            'no_kitap' => $this->faker->numerify('############'),
+            'nama_ayah' => $this->faker->name(),
+            'nama_ibu' => $this->faker->name(),
         ];
     }
 }
